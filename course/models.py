7f4b601e0595f85ff12bc9962eff4306d6d5852b
Polina <polinakiseleva4@gmail.com>
2023-08-17 16:37:04 +0500

    Создала модели курса и урока

---
+++ course/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    """Модель курса"""
+    title = models.CharField(max_length=150, verbose_name='Название курса')
+    description = models.TextField(verbose_name='Описание курса')
+    preview = models.ImageField(upload_to='course_preview/', verbose_name='Превью', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    """Модель урока"""
+    title = models.CharField(max_length=150, verbose_name='Название урока')
+    description = models.TextField(verbose_name='Описание урока')
+    preview = models.ImageField(upload_to='lesson_preview/', verbose_name='Превью', **NULLABLE)
+    video_link = models.TextField(verbose_name='Ссылка на видео', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
