5389a28f71a153149f32c8934637b22c0d2906e5
Polina <polinakiseleva4@gmail.com>
2023-08-20 01:53:15 +0500

    Реализовала привязку урока к курсу

--- course/models.py
+++ course/models.py
@@ -23,6 +23,8 @@ class Lesson(models.Model):
     description = models.TextField(verbose_name='Описание урока')
     preview = models.ImageField(upload_to='lesson_preview/', verbose_name='Превью', **NULLABLE)
     video_link = models.TextField(verbose_name='Ссылка на видео', **NULLABLE)
+    course = models.ForeignKey(Course, default=1, on_delete=models.CASCADE, related_name='lessons',
+                               verbose_name='Курс')
 
     def __str__(self):
         return f'{self.title}'
