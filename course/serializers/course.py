d55099b235230cbc479396097fcb45f57d50348c
Polina <polinakiseleva4@gmail.com>
2023-08-20 01:54:21 +0500

    Добавила вывод количества уроков и информации по ним для каждого курса

--- course/serializers/course.py
+++ course/serializers/course.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
 from course.models import Course
+from course.serializers.lesson import LessonSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    @staticmethod
+    def get_lessons_count(instance):
+        return instance.lessons.count()
