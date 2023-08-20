a0d83a83f92eec890b6191c65eecfb7c7c435195
Polina <polinakiseleva4@gmail.com>
2023-08-20 11:21:07 +0500

    Обновление внешних зависимостей

--- course/urls.py
+++ course/urls.py
@@ -6,6 +6,7 @@ from course.apps import CourseConfig
 from course.views.course import CourseViewSet
 from course.views.lesson import LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, \
     LessonDestroyAPIView
+from course.views.payments import PaymentsListAPIView, PaymentsCreateAPIView
 
 app_name = CourseConfig.name
 
@@ -18,4 +19,8 @@ urlpatterns = [
     path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-get'),
     path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
     path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
+
+    # payments
+    path('payment/create/', PaymentsCreateAPIView.as_view(), name='payment-create'),
+    path('payments/', PaymentsListAPIView.as_view(), name='payments-list'),
 ] + router.urls
